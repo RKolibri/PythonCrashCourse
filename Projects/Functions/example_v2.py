84637f3fd493c7f4fb70162bb974d71153858d48
kolibri <klevis@resulaj.eu>
2022-06-17 21:04:23 +0200

    2 new functions added as an example on example_v2.py
    Signed-off-by: kolibri <klevis@resulaj.eu>

--- Projects/Functions/example_v2.py
+++ Projects/Functions/example_v2.py
@@ -3,3 +3,20 @@ def greet_user(username):
 
 
 greet_user('Kolibri')
+
+
+def display_message():
+    print("Display message function")
+    print('In this chapter we are learning about Python functions, \n their arguments and parameters \n')
+
+
+display_message()
+
+
+def favorite_book(book_name):
+    print("Display favorite book function")
+    print(f"One of my favorite books so far is {book_name.title()} , \n actually it's the first book im reading \n in "
+          f"order to learn coding in Python language!")
+
+
+favorite_book('Python Crash Course')
