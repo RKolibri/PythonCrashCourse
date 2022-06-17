5d585186c4acf6ba0c2654350dcc5cebe30f26da
kolibri <klevis@resulaj.eu>
2022-06-17 20:50:48 +0200

    Functions examples
    Signed-off-by: kolibri <klevis@resulaj.eu>

--- Projects/Functions/example.py
+++ Projects/Functions/example.py
@@ -1,6 +1,18 @@
+from datetime import datetime, date
+
+
 def greet_user():
     """Display a simple greeting"""
     print("Hello!")
+    print("How are you today ?")
+
+    today = date.today()
+    # dd/mm/YY
+    date_today = today.strftime("%d/%m/%Y")
+    print("Today's date is :", date_today)
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    print("Current Time is :", current_time)
 
 
 greet_user()
