b38ee8cc586612c7eda2d42a93953b10a1940da5
Kolibri <klevis@resulaj.eu>
2022-06-18 20:21:25 +0200

    another exercise with Mosh
    Signed-off-by: Kolibri <klevis@resulaj.eu>

--- Projects/CodeWithMosh/greet.py
+++ Projects/CodeWithMosh/greet.py
@@ -1,6 +1,15 @@
-def greet_user():
-    print("Hi there!")
+def greet_user(firstname, lastname, age, email):
+    print(f"Hi {firstname.title()} {lastname.title()}",
+          f"you are {age}", "years old!",
+          f"Your email is: {email}")
     print("Welcome aboard")
 
 
-greet_user()
+def calc_cost(price, shipping, discount):
+    totalcost = price + shipping * discount
+    print("Total Cost is: ", totalcost, "$")
+
+
+greet_user("Klevis", "Resulaj", 26, "klevis@resulaj.eu")
+
+calc_cost(price=50, shipping=5, discount=0.5)
