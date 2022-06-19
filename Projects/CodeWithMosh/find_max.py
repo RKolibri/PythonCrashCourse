9aaf3102bcf236c8a0500e45c35c79080528233f
kolibri <klevis@resulaj.eu>
2022-06-19 19:57:11 +0200

    exercises on modules and functions
    Signed-off-by: kolibri <klevis@resulaj.eu>

---
+++ Projects/CodeWithMosh/find_max.py
@@ -0,0 +1,6 @@
+from utils import find_max
+
+numbers = [10, 3, 6, 2, 44, 53, 65]
+
+max = find_max(numbers)
+print(max)
---
+++ Projects/CodeWithMosh/modules.py
@@ -0,0 +1,5 @@
+import converters
+from converters import kg_to_lbs
+
+print(converters.lbs_to_kg(85))
+kg_to_lbs(100)
